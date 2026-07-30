5e9b2a59acba10480fd5f4fe64488d94fd9008e5
archieraghav <archieasr0204@gmail.com>
2026-07-30 11:32:13 +0530

    Remove Duplicates in-place from Sorted Array

--- step3/removeDuplicates.java
+++ step3/removeDuplicates.java
@@ -6,8 +6,24 @@
 
 package step3;
 
+import java.util.HashSet;
+
 public class removeDuplicates {
     public static void main(String[] args) {
         int [] arr = {1,1,2,2,2,3,3,4};
+        duplicates(arr);
+    }
+
+    public static void duplicates(int [] arr){
+
+        //using hashset
+        HashSet<Integer> unique = new HashSet<>();
+
+        for(int i = 0; i < arr.length; i++){
+            if(!unique.contains(arr[i])){
+                unique.add(arr[i]);
+            }
+        }
+        System.out.println(unique);
     }
 }
